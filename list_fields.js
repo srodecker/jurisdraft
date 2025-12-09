9f1855bf2aa8326644cb95cd315e6ff77e246750
Shane <Shanerodecker@gmail.com>
2025-12-08 19:10:32 -0800

    Add new EJ-001 template and update extract page

---
+++ list_fields.js
@@ -0,0 +1,14 @@
+const { PDFDocument } = require('pdf-lib');
+const fs = require('fs');
+
+async function listFields() {
+    const pdfBytes = fs.readFileSync('templates/new-ej-001.pdf');
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const form = pdfDoc.getForm();
+    const fields = form.getFields();
+    
+    console.log('Fields in new-ej-001.pdf:');
+    fields.forEach(f => console.log(f.getName()));
+}
+
+listFields();
