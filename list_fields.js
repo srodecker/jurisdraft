69c3ede5e1ecf023280df942c577468cf72a20a8
Shane <Shanerodecker@gmail.com>
2025-12-08 22:06:53 -0800

    Update extraction prompt for test-ej-001 template

--- list_fields.js
+++ list_fields.js
@@ -2,7 +2,7 @@ const { PDFDocument } = require('pdf-lib');
 const fs = require('fs');
 
 async function listFields() {
-    const pdfBytes = fs.readFileSync('templates/new-ej-001.pdf');
+    const pdfBytes = fs.readFileSync('templates/test-ej-001.pdf');
     const pdfDoc = await PDFDocument.load(pdfBytes);
     const form = pdfDoc.getForm();
     const fields = form.getFields();
